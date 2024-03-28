06a97cbe48ff2b8d0b6f9c3f311eba914a0c3c0e
nushey <zeballosnahuelfd@gmail.com>
2024-03-28 09:16:55 -0300

    gray c

--- Taller Programacion Competitva/.vscode/GrayCode.cpp
+++ Taller Programacion Competitva/.vscode/GrayCode.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <set>
 using namespace std;
 
 int main() {
+    set<string> str;
     long n;
     cin >> n;
     long m = pow(2,n);
-    string res = "";
-    for(int i = 0; i < n; i++){
-        string aux = to_string(0);
-        res+=aux;
-    }
-    bool puseUno = false;
+    string res(n,'0');
     while(m >= 1){
-        for(int i = 0; i < n; i++){
-            cout << res[i];
-        }
-        cout << endl;
-        if(!puseUno){
-            res[n-1] = '1';
-            puseUno = true;
-        }
-        else{
-            res[n-1] = '0';
-            puseUno = false;
-        }
+        cout << res << endl;
+        str.insert(res);
+        for(int i = n-1; i >= 0; i--){
+            if(res[i] == '0'){
+                string aux = res;
+                aux[i] = '1';
+                if (str.find(aux) == str.end()) {
+                    i = -5;
+                    res = aux;
+                }   
+
+            }
+            else{
+                string aux = res;
+                aux[i] = '0';
+                if (str.find(aux) == str.end()) {
+                    i = -5;
+                    res = aux;
+                } 
+            }
+        }   
         m--;
     }
 }
